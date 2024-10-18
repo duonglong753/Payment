33f3c40576ec50e708da15a61a59f0eb9e2fb128
Hung Long <duonglong753@gmail.com>
2024-10-18 14:04:22 +0700

    create api controller

--- Payment/src/pre/Payment.Api/Program.cs
+++ Payment/src/pre/Payment.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "Payment Service API",
+        Description = "Sample .NET payment api",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Name = "Cafe Dev Code",
+            Url = new Uri("https://cafedevcode.com")
+        }
+    });
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var path = Path.Combine(AppContext.BaseDirectory, xmlFileName);
+    options.IncludeXmlComments(path);
+});
 
 var app = builder.Build();
 
--- Payment/src/pre/Payment.Api/obj/Debug/net7.0/Payment.Api.AssemblyInfo.cs
+++ Payment/src/pre/Payment.Api/obj/Debug/net7.0/Payment.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Payment.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357c7918210d90edafd450765dac5f480927e8cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Payment.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Payment.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
