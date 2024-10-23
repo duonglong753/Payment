2af3e32a4dd791dc4632f25fcb1b60222a6f08a7
Hung Long <duonglong753@gmail.com>
2024-10-23 11:20:02 +0700

    init feature merchant

---
+++ Payment/src/core/Payment.Application/Features/Merchant/DTOs/MerchantDtos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Application.Features.DTOs
+{
+    public class MerchantDtos 
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? MerchantName { get; set; } = string.Empty;
+        public string? MerchantWebLink { get; set; } = string.Empty;
+        public string? MerchantInpUrl { get; set; } = string.Empty;
+        public string? MerchantReturnUrl { get; set; } = string.Empty;
+        public string? SecretKey { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
