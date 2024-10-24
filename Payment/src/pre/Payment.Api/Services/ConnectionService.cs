53c2ec54977956b2790a99454636e1b6abac0446
Hung Long <duonglong753@gmail.com>
2024-10-24 15:25:26 +0700

    add current user service & connection service

---
+++ Payment/src/pre/Payment.Api/Services/ConnectionService.cs
@@ -0,0 +1,14 @@
+﻿using Payment.Application.Interface;
+
+namespace Payment.Api.Services
+{
+    public class ConnectionService : IConnectionService
+    {
+        private readonly IConfiguration _configuration;
+        public ConnectionService(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        public string? Database => _configuration.GetConnectionString("Database");
+    }
+}
---
+++ Payment/src/pre/Payment.Api/Services/CurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using Payment.Application.Interface;
+using System.Security.Claims;
+
+namespace Payment.Api.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            this._contextAccessor = httpContextAccessor;
+        }
+        string? ICurrentUserService.UserId => _contextAccessor?.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
