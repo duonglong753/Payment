f0ff4864f390b338f905ab1bebf5f96c68aae362
Hung Long <duonglong753@gmail.com>
2024-10-24 15:17:10 +0700

    add new method CreateMerchantHandler

--- Payment/src/core/Payment.Application/Features/Merchant/Commands/CreateMerchant.cs
+++ Payment/src/core/Payment.Application/Features/Merchant/Commands/CreateMerchant.cs
@@ -1,16 +1,82 @@
-﻿using System;
+﻿using Mapster;
+using MediatR;
+using Payment.Application.Base.Models;
+using Payment.Application.Constants;
+using Payment.Application.Features.DTOs;
+using Payment.Application.Interface;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Payment.Application.Features.Merchant.Commands
+namespace Payment.Application.Features.Commands
 {
-    public class CreateMerchant
+    public class CreateMerchant : IRequest<BaseResultWithData<MerchantDtos>>
     {
         public string? MerchantName { get; set; } = string.Empty;
         public string? MerchantWebLink { get; set; } = string.Empty;
         public string? MerchantInpUrl { get; set; } = string.Empty;
         public string? MerchantReturnUrl { get; set; } = string.Empty;
+    }
+    public class CreateMerchantHandler : IRequestHandler<CreateMerchant, BaseResultWithData<MerchantDtos>>
+    {
+        private readonly ISqlService sqlService;
+        private readonly IConnectionService connectionService;
+        private readonly ICurrentUserService currentUserService;
+        public CreateMerchantHandler(ICurrentUserService currentUserService, ISqlService sqlService, IConnectionService connectionService)
+        {
+            this.sqlService = sqlService;
+            this.connectionService = connectionService;
+            this.currentUserService = currentUserService;
+        }
+        public Task<BaseResultWithData<MerchantDtos>> Handle(CreateMerchant request, CancellationToken cancellationToken)
+        {
+            var result = new BaseResultWithData<MerchantDtos>();
+            try
+            {
+                string connectionString = connectionService.Database ?? string.Empty;
+                var outputIdParam = sqlService.CreateOutputParameter("@InsertedId", SqlDbType.NVarChar, 50);
+                var parameters = new SqlParameter[]
+                {
+                    new SqlParameter("@MerchantName", request.MerchantName ?? string.Empty),
+                    new SqlParameter("@MerchantWebLink", request.MerchantWebLink?? string.Empty),
+                    new SqlParameter("@MerchantInpUrl", request.MerchantInpUrl ?? string.Empty),
+                    new SqlParameter("@MerchantReturnUrl", request.MerchantReturnUrl ?? string.Empty),
+                    new SqlParameter("@InsertUser", currentUserService.UserId ?? string.Empty),
+                    outputIdParam
+                };
+                (int affectedRows, string sqlError) = sqlService.ExecuteNonQuery(connectionString, MerchantConstants.InsertSprocName, parameters);
+                if (affectedRows == 1)
+                {
+                    var resultData = request.Adapt<MerchantDtos>();
+                    resultData.Id = outputIdParam.Value?.ToString() ?? string.Empty;
+                    result.Set(true, MessageConstants.OK, resultData);
+                }
+                else
+                {
+                    result.Set(false, MessageConstants.Error);
+                    result.Errors.Add(new BaseError()
+                    {
+                        Code = "Sql",
+                        Message = sqlError
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Set(false, MessageConstants.Error);
+                result.Errors.Add(new BaseError()
+                {
+                    Code = MessageConstants.Exception,
+                    Message = ex.Message,
+                });
+            }
+
+
+            return Task.FromResult(result);
+        }
     }
 }
